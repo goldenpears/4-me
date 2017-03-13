783226f2b9e121f2775906bc76957279bdd67e87
locovna <locovna@gmail.com>
2017-03-13 17:48:11 +0200

    create menu and base activities

--- 4me/app/src/main/java/com/locovna/a4me/MainActivity.java
+++ 4me/app/src/main/java/com/locovna/a4me/MainActivity.java
@@ -1,13 +1,49 @@
 package com.locovna.a4me;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends Activity {
+  public static final String TAG = MainActivity.class.getSimpleName();
+
+  @BindView (R.id.hotseat_button) Button hotseatButton;
+  @BindView (R.id.vs_ai_button) Button vsAiButton;
+  @BindView (R.id.about_button) Button aboutButton;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    ButterKnife.bind(this);
+
+    hotseatButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        Intent intent = new Intent(MainActivity.this, HotseatActivity.class);
+        startActivity(intent);
+      }
+    });
+
+    vsAiButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        Intent intent = new Intent(MainActivity.this, AiActivity.class);
+        startActivity(intent);
+      }
+    });
+
+    aboutButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+        startActivity(intent);
+      }
+    });
   }
 }
